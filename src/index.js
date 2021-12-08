e57665a534411a55b819263aa4b172b186f8ecfe
Russell Moffitt <russmo@coralhead.com>
2021-12-08 12:18:49 -0800

    customized for MCI Gala 2021

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function App() {
 
     useEffect(() => {
 
-        setLoading(true)
+        // setLoading(true)
           
         fetch('/api')
         .then(response => response.json())
@@ -33,6 +33,8 @@ function App() {
     var formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
+        maximumFractionDigits: 0, 
+        minimumFractionDigits: 0,
     })
 
     const now = data.raised/data.goal*100
@@ -40,20 +42,21 @@ function App() {
     const progressInstance = (
         !loading ?
         <div>
-            <div style={{display:"flex", justifyContent:"space-between"}}>
-                <div><span style={{fontSize:"xx-large"}}>{formatter.format(data.raised)}</span> {data.donors} supporters</div>
-                <div><span style={{fontSize:"xx-large"}}>{formatter.format(data.goal)}</span></div>
-            </div>
-            <ProgressBar>
-                <ProgressBar now={now} label={`${now.toFixed(1)}%`} animated key={1} />
+            <ProgressBar style={{height:"40px"}}>
+                <ProgressBar now={now} label="" animated key={1} />
                 <ProgressBar variant={"customBar"} now={100-now} key={2} color="gray" />
             </ProgressBar>
+            <div class={"progress-amounts"} style={{display:"flex", justifyContent:"space-between", marginTop:"-2.75em", paddingLeft:"8px", paddingRight:"8px"}}>
+                <div><span class={"raised"} style={{fontSize:"xx-large"}}>{formatter.format(data.raised)}</span> <span class={"supporters"} style={{marginLeft:"60px"}}>{data.donors} supporters</span></div>
+                <div><span class={"goal"} style={{fontSize:"xx-large"}}>{formatter.format(data.goal)}</span></div>
+            </div>
+            
         </div>
         : <div>Loading...</div>
     );
 
     return (
-        <div style={{width:"75%", margin:"5% auto"}}>
+        <div style={{width:"85%", margin:"5% auto"}}>
             {progressInstance}
         </div>
     )
